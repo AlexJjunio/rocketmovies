0f059f992f6da9f0ccfb6f3e8c3cb09ffdd00a4e
iJjunio <alex_jjunio@outlook.com>
2023-07-25 05:51:36 -0300

    Add components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,34 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tags"
+import { ButtonText } from "../../components/ButtonText"
 
 
 export function Details() {
   return(
     <Container>
       <Header/>
+
+      <ButtonText title="Excluir nota"/>
+
+      {/* <Section title="Marcadores">
+        <Links>
+          <li>
+            <a href="#">Super-man</a>
+          </li>
+          <li>
+            <a href="#">Super-man</a>
+          </li>
+        </Links>
+      </Section> */}
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="node"/>
+      </Section>
+
       <Button title="Voltar"/>
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,6 +10,17 @@ export const Container = styled.div`
     "header"
     "content"
   ;
+`;
 
+export const Links = styled.ul`
+  list-style: none;
 
+  > li {
+    margin-top: 12px;
+
+    a {
+      color: ${({theme}) => theme.COLORS.WHITE};
+    }
+
+  }
 `;
